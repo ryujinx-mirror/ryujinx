ee6794e3972620dcfc69c7fa1238ef5822ea075c
Ac_K <Acoustik666@gmail.com>
2018-04-18 01:39:27 +0000

    Update IAudioController.cs (#86)
    * Update IAudioController.cs
    
    Stubs:
    - SetExpectedMasterVolume
    - GetMainAppletExpectedMasterVolume
    - GetLibraryAppletExpectedMasterVolume
    - ChangeMainAppletMasterVolume
    - SetTransparentVolumeRate
    
    * Update IAudioController.cs

--- Ryujinx.Core/OsHle/Services/Am/IAudioController.cs
+++ Ryujinx.Core/OsHle/Services/Am/IAudioController.cs
@@ -13,8 +13,59 @@ namespace Ryujinx.Core.OsHle.Services.Am
         {
             m_Commands = new Dictionary<int, ServiceProcessRequest>()
             {
-                //...
+                { 0, SetExpectedMasterVolume              },
+                { 1, GetMainAppletExpectedMasterVolume    },
+                { 2, GetLibraryAppletExpectedMasterVolume },
+                { 3, ChangeMainAppletMasterVolume         }, 
+                { 4, SetTransparentVolumeRate             }
             };
+        }
+
+        public long SetExpectedMasterVolume(ServiceCtx Context)
+        {
+            float Unknown0 = Context.RequestData.ReadSingle();
+            float Unknown1 = Context.RequestData.ReadSingle();
+
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
+        }
+
+        public long GetMainAppletExpectedMasterVolume(ServiceCtx Context)
+        {
+            Context.ResponseData.Write(1f);
+
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
+        }
+
+        public long GetLibraryAppletExpectedMasterVolume(ServiceCtx Context)
+        {
+            Context.ResponseData.Write(1f);
+
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
+        }
+
+        public long ChangeMainAppletMasterVolume(ServiceCtx Context)
+        {
+            float Unknown0 = Context.RequestData.ReadSingle();
+            long  Unknown1 = Context.RequestData.ReadInt64();
+
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
+        }
+
+        public long SetTransparentVolumeRate(ServiceCtx Context)
+        {
+            float Unknown0 = Context.RequestData.ReadSingle();
+
+            Logging.Stub(LogClass.ServiceAm, "Stubbed");
+
+            return 0;
         }
     }
 }
