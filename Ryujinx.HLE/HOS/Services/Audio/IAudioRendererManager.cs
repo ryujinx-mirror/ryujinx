dead89576e0fc8703398f82de1697bec132f0267
Ac_K <Acoustik666@gmail.com>
2019-11-14 10:06:28 +0100

    Fix GetAudioDeviceServiceWithRevisionInfo logging (#822)
    * Fix GetAudioDeviceServiceWithRevisionInfo logging
    
    `revisionInfo` and `appletResourceUserId` were read in the wrong order.
    
    * Fix comment

--- Ryujinx.HLE/HOS/Services/Audio/IAudioRendererManager.cs
+++ Ryujinx.HLE/HOS/Services/Audio/IAudioRendererManager.cs
@@ -134,11 +134,11 @@ namespace Ryujinx.HLE.HOS.Services.Audio
         }
 
         [Command(4)] // 4.0.0+
-        // GetAudioDeviceServiceWithRevisionInfo(nn::applet::AppletResourceUserId, u32) -> object<nn::audio::detail::IAudioDevice>
+        // GetAudioDeviceServiceWithRevisionInfo(u32 revision_info, nn::applet::AppletResourceUserId) -> object<nn::audio::detail::IAudioDevice>
         public ResultCode GetAudioDeviceServiceWithRevisionInfo(ServiceCtx context)
         {
-            long appletResourceUserId = context.RequestData.ReadInt64();
             int  revisionInfo         = context.RequestData.ReadInt32();
+            long appletResourceUserId = context.RequestData.ReadInt64();
 
             Logger.PrintStub(LogClass.ServiceAudio, new { appletResourceUserId, revisionInfo });
 
