cfe5fec0cf312d7a5f49470e93b6e9f83e883e21
gdkchan <gab.dark.100@gmail.com>
2019-12-11 14:48:08 -0300

    Remove some usings that are not needed

--- Ryujinx.Graphics.Shader/Instructions/InstEmitVideo.cs
+++ Ryujinx.Graphics.Shader/Instructions/InstEmitVideo.cs
@@ -1,9 +1,7 @@
 using Ryujinx.Graphics.Shader.Decoders;
-using Ryujinx.Graphics.Shader.IntermediateRepresentation;
 using Ryujinx.Graphics.Shader.Translation;
 
 using static Ryujinx.Graphics.Shader.Instructions.InstEmitHelper;
-using static Ryujinx.Graphics.Shader.IntermediateRepresentation.OperandHelper;
 
 namespace Ryujinx.Graphics.Shader.Instructions
 {
