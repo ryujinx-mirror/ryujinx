b0f0bd205dc1664aa18cb2fa61eeac929e779d36
VolcaEM <63682805+VolcaEM@users.noreply.github.com>
2020-05-03 22:45:22 +0200

    Add various error codes to Audio (#1201)
    The error codes were taken from Switchbrew (switchbrew.org/wiki/Error_codes)

--- Ryujinx.HLE/HOS/Services/Audio/ResultCode.cs
+++ Ryujinx.HLE/HOS/Services/Audio/ResultCode.cs
@@ -10,6 +10,12 @@ namespace Ryujinx.HLE.HOS.Services.Audio
         DeviceNotFound        = (1 << ErrorCodeShift) | ModuleId,
         UnsupportedRevision   = (2 << ErrorCodeShift) | ModuleId,
         UnsupportedSampleRate = (3 << ErrorCodeShift) | ModuleId,
-        OpusInvalidInput      = (6 << ErrorCodeShift) | ModuleId
+        BufferSizeTooSmall    = (4 << ErrorCodeShift) | ModuleId,
+        OpusInvalidInput      = (6 << ErrorCodeShift) | ModuleId,
+        TooManyBuffersInUse   = (8 << ErrorCodeShift) | ModuleId,
+        InvalidChannelCount   = (10 << ErrorCodeShift) | ModuleId,
+        InvalidOperation      = (513 << ErrorCodeShift) | ModuleId,
+        InvalidHandle         = (1536 << ErrorCodeShift) | ModuleId,
+        OutputAlreadyStarted  = (1540 << ErrorCodeShift) | ModuleId
     }
 }
