fe72bc9274182f9777321637444c0637bb7c5c95
VolcaEM <63682805+VolcaEM@users.noreply.github.com>
2020-05-08 16:06:32 +0200

    Add various error codes to NCM LR (#1225)
    The error codes were taken from Switchbrew (https://switchbrew.org/wiki/Error_codes)

--- Ryujinx.HLE/HOS/Services/Ncm/Lr/ResultCode.cs
+++ Ryujinx.HLE/HOS/Services/Ncm/Lr/ResultCode.cs
@@ -7,7 +7,14 @@
 
         Success = 0,
 
-        ProgramLocationEntryNotFound = (2 << ErrorCodeShift) | ModuleId,
-        AccessDenied                 = (5 << ErrorCodeShift) | ModuleId
+        ProgramLocationEntryNotFound           = (2  << ErrorCodeShift) | ModuleId,
+        InvalidContextForControlLocation       = (3  << ErrorCodeShift) | ModuleId,
+        StorageNotFound                        = (4  << ErrorCodeShift) | ModuleId,
+        AccessDenied                           = (5  << ErrorCodeShift) | ModuleId,
+        OfflineManualHTMLLocationEntryNotFound = (6  << ErrorCodeShift) | ModuleId,
+        TitleIsNotRegistered                   = (7  << ErrorCodeShift) | ModuleId,
+        ControlLocationEntryForHostNotFound    = (8  << ErrorCodeShift) | ModuleId,
+        LegalInfoHTMLLocationEntryNotFound     = (9  << ErrorCodeShift) | ModuleId,
+        ProgramLocationForDebugEntryNotFound   = (10 << ErrorCodeShift) | ModuleId
     }
 }
