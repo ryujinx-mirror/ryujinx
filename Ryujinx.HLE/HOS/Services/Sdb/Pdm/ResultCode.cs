1dd78142fa8ba4347272d1dbdd3690e7ea27c50c
VolcaEM <63682805+VolcaEM@users.noreply.github.com>
2020-05-09 10:57:19 +0200

    Add two error codes to SDB PDM (#1230)
    * Add two error codes to SDB PDM
    
    The error codes were taken from Switchbrew (https://switchbrew.org/wiki/Error_codes)
    
    * Remove empty line

--- Ryujinx.HLE/HOS/Services/Sdb/Pdm/ResultCode.cs
+++ Ryujinx.HLE/HOS/Services/Sdb/Pdm/ResultCode.cs
@@ -7,7 +7,9 @@
 
         Success = 0,
 
+        InvalidUserID      = (100 << ErrorCodeShift) | ModuleId,
         UserNotFound       = (101 << ErrorCodeShift) | ModuleId,
-        ServiceUnavailable = (150 << ErrorCodeShift) | ModuleId
+        ServiceUnavailable = (150 << ErrorCodeShift) | ModuleId,
+        FileStorageFailure = (200 << ErrorCodeShift) | ModuleId
     }
 }
