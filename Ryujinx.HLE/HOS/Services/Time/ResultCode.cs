b96aa7574f7f1d6072148dfb38746af0adf6c16e
VolcaEM <63682805+VolcaEM@users.noreply.github.com>
2020-05-13 07:28:53 +0200

    Add missing error code to Time (#1237)
    The error code was taken from Switchbrew (https://switchbrew.org/wiki/Error_codes)
    
    Even if TimeServiceNotInitialized's "description" is 0, the result "value" of "(0 << ErrorCodeShift) | ModuleId" is 0x74 so it is not the same as "Success" (0)

--- Ryujinx.HLE/HOS/Services/Time/ResultCode.cs
+++ Ryujinx.HLE/HOS/Services/Time/ResultCode.cs
@@ -7,15 +7,16 @@
 
         Success = 0,
 
-        PermissionDenied         = (1   << ErrorCodeShift) | ModuleId,
-        TimeMismatch             = (102 << ErrorCodeShift) | ModuleId,
-        UninitializedClock       = (103 << ErrorCodeShift) | ModuleId,
-        TimeNotFound             = (200 << ErrorCodeShift) | ModuleId,
-        Overflow                 = (201 << ErrorCodeShift) | ModuleId,
-        LocationNameTooLong      = (801 << ErrorCodeShift) | ModuleId,
-        OutOfRange               = (902 << ErrorCodeShift) | ModuleId,
-        TimeZoneConversionFailed = (903 << ErrorCodeShift) | ModuleId,
-        TimeZoneNotFound         = (989 << ErrorCodeShift) | ModuleId,
-        NotImplemented           = (990 << ErrorCodeShift) | ModuleId,
+        TimeServiceNotInitialized = (0   << ErrorCodeShift) | ModuleId,
+        PermissionDenied          = (1   << ErrorCodeShift) | ModuleId,
+        TimeMismatch              = (102 << ErrorCodeShift) | ModuleId,
+        UninitializedClock        = (103 << ErrorCodeShift) | ModuleId,
+        TimeNotFound              = (200 << ErrorCodeShift) | ModuleId,
+        Overflow                  = (201 << ErrorCodeShift) | ModuleId,
+        LocationNameTooLong       = (801 << ErrorCodeShift) | ModuleId,
+        OutOfRange                = (902 << ErrorCodeShift) | ModuleId,
+        TimeZoneConversionFailed  = (903 << ErrorCodeShift) | ModuleId,
+        TimeZoneNotFound          = (989 << ErrorCodeShift) | ModuleId,
+        NotImplemented            = (990 << ErrorCodeShift) | ModuleId,
     }
 }
