da3a53a29cadd0bd56c5e0585e31cfa0d8479f9a
Ac_K <Acoustik666@gmail.com>
2020-09-20 05:32:48 +0200

    caps: Stub SetShimLibraryVersion (#1552)

--- Ryujinx.HLE/HOS/Services/Caps/IAlbumApplicationService.cs
+++ Ryujinx.HLE/HOS/Services/Caps/IAlbumApplicationService.cs
@@ -1,8 +1,22 @@
+using Ryujinx.Common.Logging;
+
 namespace Ryujinx.HLE.HOS.Services.Caps
 {
     [Service("caps:u")]
     class IAlbumApplicationService : IpcService
     {
         public IAlbumApplicationService(ServiceCtx context) { }
+
+        [Command(32)] // 7.0.0+
+        // SetShimLibraryVersion(pid, u64, nn::applet::AppletResourceUserId)
+        public ResultCode SetShimLibraryVersion(ServiceCtx context)
+        {
+            ulong shimLibraryVersion   = context.RequestData.ReadUInt64();
+            ulong appletResourceUserId = context.RequestData.ReadUInt64();
+
+            Logger.Stub?.PrintStub(LogClass.ServiceCaps, new { shimLibraryVersion, appletResourceUserId });
+
+            return ResultCode.Success;
+        }
     }
 }
--- Ryujinx.HLE/HOS/Services/Caps/IScreenShotApplicationService.cs
+++ Ryujinx.HLE/HOS/Services/Caps/IScreenShotApplicationService.cs
@@ -1,8 +1,22 @@
-﻿namespace Ryujinx.HLE.HOS.Services.Caps
+﻿using Ryujinx.Common.Logging;
+
+namespace Ryujinx.HLE.HOS.Services.Caps
 {
     [Service("caps:su")] // 6.0.0+
     class IScreenShotApplicationService : IpcService
     {
         public IScreenShotApplicationService(ServiceCtx context) { }
+
+        [Command(32)] // 7.0.0+
+        // SetShimLibraryVersion(pid, u64, nn::applet::AppletResourceUserId)
+        public ResultCode SetShimLibraryVersion(ServiceCtx context)
+        {
+            ulong shimLibraryVersion   = context.RequestData.ReadUInt64();
+            ulong appletResourceUserId = context.RequestData.ReadUInt64();
+
+            Logger.Stub?.PrintStub(LogClass.ServiceCaps, new { shimLibraryVersion, appletResourceUserId });
+
+            return ResultCode.Success;
+        }
     }
 }
