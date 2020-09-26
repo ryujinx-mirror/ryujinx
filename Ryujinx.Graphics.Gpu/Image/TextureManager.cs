f89b754abb3d721c09ba6ab00f7906d2d5ccb02b
riperiperi <rhy3756547@hotmail.com>
2020-09-27 00:37:45 +0100

    Always set new texture data for textures initialized by a copy. (#1576)

--- Ryujinx.Graphics.Gpu/Image/TextureManager.cs
+++ Ryujinx.Graphics.Gpu/Image/TextureManager.cs
@@ -771,7 +771,7 @@ namespace Ryujinx.Graphics.Gpu.Image
                 // Any textures that are incompatible will contain garbage data, so they should be removed where possible.
 
                 int viewCompatible = 0;
-                bool setData = isSamplerTexture || overlapsCount == 0;
+                bool setData = isSamplerTexture || overlapsCount == 0 || flags.HasFlag(TextureSearchFlags.ForCopy);
 
                 for (int index = 0; index < overlapsCount; index++)
                 {
