73118dcb989425ed0ea255d3419028ec21fa23e8
Mary <me@thog.eu>
2021-01-01 00:10:44 +0100

    amadeus: Update copyright year
    Happy new Year

--- Ryujinx.Audio.Renderer/Server/Types/AudioRendererExecutionMode.cs
+++ Ryujinx.Audio.Renderer/Server/Types/AudioRendererExecutionMode.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2019-2020 Ryujinx
+// Copyright (c) 2019-2021 Ryujinx
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
