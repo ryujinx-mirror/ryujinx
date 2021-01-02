3e7383b3fdf78d88a25dc2f5d285a847596158f4
Bruno Macabeus <macabeus@users.noreply.github.com>
2021-01-02 07:11:38 -0800

    services: add "apm:p" (#1854)
    * services: add "apm:p"
    
    * review: change amp:p version range
    
    * review: remove uneeded calls
    
    * review: fix ImanagerPrivileged

---
+++ Ryujinx.HLE/HOS/Services/Apm/IManagerPrivileged.cs
@@ -0,0 +1,19 @@
+namespace Ryujinx.HLE.HOS.Services.Apm
+{
+    // NOTE: This service doesn’t exist anymore after firmware 7.0.1. But some outdated homebrew still uses it.
+
+    [Service("apm:p")] // 1.0.0-7.0.1
+    class IManagerPrivileged : IpcService
+    {
+        public IManagerPrivileged(ServiceCtx context) { }
+
+        [Command(0)]
+        // OpenSession() -> object<nn::apm::ISession>
+        public ResultCode OpenSession(ServiceCtx context)
+        {
+            MakeObject(context, new SessionServer(context));
+
+            return ResultCode.Success;
+        }
+    }
+}
