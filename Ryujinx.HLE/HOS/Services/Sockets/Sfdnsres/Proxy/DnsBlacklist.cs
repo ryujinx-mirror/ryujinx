4443e1890958cc01b523207736f74c58767f4017
mageven <62494521+mageven@users.noreply.github.com>
2021-03-30 04:25:53 +0530

    Patch up DNS Blacklist (#2153)
    Make the regex patterns case insensitive for robustness

--- Ryujinx.HLE/HOS/Services/Sockets/Sfdnsres/Proxy/DnsBlacklist.cs
+++ Ryujinx.HLE/HOS/Services/Sockets/Sfdnsres/Proxy/DnsBlacklist.cs
@@ -4,12 +4,14 @@ namespace Ryujinx.HLE.HOS.Services.Sockets.Sfdnsres.Proxy
 {
     static class DnsBlacklist
     {
+        const RegexOptions RegexOpts = RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled;
+
         private static readonly Regex[] BlockedHosts = new Regex[]
         {
-            new Regex(@"^g(.*)\-lp1\.s\.n\.srv\.nintendo\.net$"),
-            new Regex(@"^(.*)\-sb\-api\.accounts\.nintendo\.com$"),
-            new Regex(@"^(.*)\-sb\.accounts\.nintendo\.com$"),
-            new Regex(@"^accounts\.nintendo\.com$")
+            new Regex(@"^g(.*)\-lp1\.s\.n\.srv\.nintendo\.net$", RegexOpts),
+            new Regex(@"^(.*)\-sb\-api\.accounts\.nintendo\.com$", RegexOpts),
+            new Regex(@"^(.*)\-sb\.accounts\.nintendo\.com$", RegexOpts),
+            new Regex(@"^accounts\.nintendo\.com$", RegexOpts)
         };
 
         public static bool IsHostBlocked(string host)
