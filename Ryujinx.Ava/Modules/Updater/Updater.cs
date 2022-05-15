b8fc97adf250444e75328fee4e768e28f45ebf59
Mary <mary@mary.zone>
2022-05-15 21:01:01 +0200

    Fix Avalonia updater

--- Ryujinx.Ava/Modules/Updater/Updater.cs
+++ Ryujinx.Ava/Modules/Updater/Updater.cs
@@ -98,7 +98,7 @@ namespace Ryujinx.Modules
                         string assetState = (string)asset["state"];
                         string downloadURL = (string)asset["browser_download_url"];
 
-                        if (assetName.StartsWith("ava-ryujinx") && assetName.EndsWith(_platformExt))
+                        if (assetName.StartsWith("test-ava-ryujinx") && assetName.EndsWith(_platformExt))
                         {
                             _buildUrl = downloadURL;
 
