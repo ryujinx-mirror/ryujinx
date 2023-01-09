8734ea9dd4e67c971a078f4f10c16e40476ece52
TSRBerry <20988865+TSRBerry@users.noreply.github.com>
2023-01-09 22:58:51 +0100

    Linux: Add Avalonia detection to Ryujinx.sh (#4224)
    * Revert "ava: Fix regression caused by #4013 (#4222)"
    
    This reverts commit b9f2a96595b2721836d2b73e005640f9c3bfaf23.
    
    * linux: Detect Ryujinx.Ava and don't rename the Ryujinx.Ava assembly

--- distribution/linux/Ryujinx.sh
+++ distribution/linux/Ryujinx.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(dirname $(realpath $0))
+RYUJINX_BIN="Ryujinx"
 
-env DOTNET_EnableAlternateStackCheck=1 "$SCRIPT_DIR/Ryujinx" "$@"
+if [ -f "$SCRIPT_DIR/Ryujinx.Ava" ]; then
+    RYUJINX_BIN="Ryujinx.Ava"
+fi
+
+env DOTNET_EnableAlternateStackCheck=1 "$SCRIPT_DIR/$RYUJINX_BIN" "$@"
