8c720783f50051b28188a330f87d3b74bbe26cc7
TSRBerry <20988865+TSRBerry@users.noreply.github.com>
2023-01-10 00:45:46 +0100

    linux: Fix packaging step for CI & Add Ryujinx.Headless.SDL2 to Ryujinx.sh (#4249)

--- distribution/linux/Ryujinx.sh
+++ distribution/linux/Ryujinx.sh
@@ -7,4 +7,8 @@ if [ -f "$SCRIPT_DIR/Ryujinx.Ava" ]; then
     RYUJINX_BIN="Ryujinx.Ava"
 fi
 
+if [ -f "$SCRIPT_DIR/Ryujinx.Headless.SDL2" ]; then
+    RYUJINX_BIN="Ryujinx.Headless.SDL2"
+fi
+
 env DOTNET_EnableAlternateStackCheck=1 "$SCRIPT_DIR/$RYUJINX_BIN" "$@"
