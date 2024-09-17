ccf96bf5e673456ec80f72725e4c9afa4e4c5a85
TSRBerry <20988865+TSRBerry@users.noreply.github.com>
2024-09-17 20:42:00 +0200

    Wait for async task to complete (#7122)
    This way exceptions thrown during the execution of CheckLaunchState()
    will correctly invoke the unhandled exception handler
    and cause Ryujinx to crash.

--- src/Ryujinx/UI/Windows/MainWindow.axaml.cs
+++ src/Ryujinx/UI/Windows/MainWindow.axaml.cs
@@ -482,9 +482,7 @@ namespace Ryujinx.Ava.UI.Windows
                 LoadApplications();
             }
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            CheckLaunchState();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            CheckLaunchState().Wait();
         }
 
         private void SetMainContent(Control content = null)
