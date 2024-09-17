062ef43eb46ffec0ddc441907d38da3cfc235f09
gdkchan <gab.dark.100@gmail.com>
2024-09-17 16:25:26 -0300

    Revert "Wait for async task to complete (#7122)" (#7318)
    This reverts commit ccf96bf5e673456ec80f72725e4c9afa4e4c5a85.

--- src/Ryujinx/UI/Windows/MainWindow.axaml.cs
+++ src/Ryujinx/UI/Windows/MainWindow.axaml.cs
@@ -482,7 +482,9 @@ namespace Ryujinx.Ava.UI.Windows
                 LoadApplications();
             }
 
-            CheckLaunchState().Wait();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            CheckLaunchState();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         private void SetMainContent(Control content = null)
