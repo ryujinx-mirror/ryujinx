fa7a57758725913470af1014826a9e35d1cfeddd
reggie <reggie@latte.to>
2024-10-02 02:49:42 -0500

    Fix MacOS release build.. lol

--- distribution/macos/create_macos_build_ava.sh
+++ distribution/macos/create_macos_build_ava.sh
@@ -35,7 +35,7 @@ EXECUTABLE_SUB_PATH=Contents/MacOS/Ryujinx
 rm -rf "$TEMP_DIRECTORY"
 mkdir -p "$TEMP_DIRECTORY"
 
-DOTNET_COMMON_ARGS=(-p:DebugType=embedded -p:Version="$VERSION" -p:SourceRevisionId="$SOURCE_REVISION_ID" --self-contained true $EXTRA_ARGS)
+DOTNET_COMMON_ARGS=(-p:DebugType=embedded -p:SourceRevisionId="$SOURCE_REVISION_ID" --self-contained true $EXTRA_ARGS)
 
 dotnet restore
 dotnet build -c "$CONFIGURATION" src/Ryujinx
