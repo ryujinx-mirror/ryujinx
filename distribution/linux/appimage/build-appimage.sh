aa34084ba18e8c9c9ce183a707cd54f40e97752c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-06 02:34:01 -0400

    infra: Add AppImage build workflow (#28)
    Co-authored-by: reggie <reggie@latte.to>

---
+++ distribution/linux/appimage/build-appimage.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+ROOTDIR="$(readlink -f "$(dirname "$0")")"/../../../
+cd "$ROOTDIR"
+
+BUILDDIR=${BUILDDIR:-publish}
+OUTDIR=${OUTDIR:-publish_appimage}
+
+rm -rf AppDir
+mkdir -p AppDir/usr/bin/bin
+
+# Ensure necessary bins are set as executable
+chmod +x "$BUILDDIR"/Ryujinx*
+
+# Add symlinks for the AppImage
+ln -s "$ROOTDIR"/distribution/linux/Ryujinx.desktop AppDir/Ryujinx.desktop
+ln -s "$ROOTDIR"/distribution/linux/appimage/AppRun AppDir/AppRun
+ln -s "$ROOTDIR"/distribution/misc/Logo.svg AppDir/Ryujinx.svg
+
+cp -r "$BUILDDIR"/* AppDir/usr/bin/
+
+mkdir -p "$OUTDIR"
+
+appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 21 \
+    -u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|Ryujinx|latest|*.AppImage.zsync" \
+    AppDir "$OUTDIR"/Ryujinx.AppImage
+
+# ??
+mv ./*.AppImage.zsync "$OUTDIR"
