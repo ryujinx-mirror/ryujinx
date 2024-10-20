e5198d17b19f77f7a3e8eb27329bd0219803da71
EmulationEnjoyer <144477224+EmulationEnjoyer@users.noreply.github.com>
2024-10-20 16:06:05 +0000

    fix whitespace complaint

--- src/Ryujinx.Graphics.Vulkan/VertexBufferState.cs
+++ src/Ryujinx.Graphics.Vulkan/VertexBufferState.cs
@@ -55,10 +55,10 @@ namespace Ryujinx.Graphics.Vulkan
             if (_handle != BufferHandle.Null)
             {
                 // May need to restride the vertex buffer.
-
+                // 
                 // Fix divide by zero when recovering from missed draw (Oct. 16 2024)
                 // (fixes crash in 'Baldo: The Guardian Owls' opening cutscene)
-                if (gd.NeedsVertexBufferAlignment(AttributeScalarAlignment, out int alignment) && alignment != 0 && (_stride % alignment) != 0) 
+                if (gd.NeedsVertexBufferAlignment(AttributeScalarAlignment, out int alignment) && alignment != 0 && (_stride % alignment) != 0)
                 {
                     autoBuffer = gd.BufferManager.GetAlignedVertexBuffer(cbs, _handle, _offset, _size, _stride, alignment);
 
